a1ec774dd4f521f9260f3872f6619f40a29a204b
RodrigoAlejandroBP <rbravop97@gmail.com>
2020-08-02 18:08:11 -0400

    casi listo animaciones y botones

--- TrabajoWebSocket/static/script.js
+++ TrabajoWebSocket/static/script.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     var a=[]; 
     var yo;
     var listadeusuarios=[];
-
+    var terminaconv=[];
+    var terminaconv2=[];
     
     //inicializar el puerto
     var socket = io.connect('http://127.0.0.1:5000');
@@ -37,7 +38,17 @@ $(document).ready(function() {
     }); */
     
   
+    function ocultaratodos(emisor,receptor){
+        for (var i = 0; i < listadeusuarios.length; i++) {
+            console.log(receptor );
+            console.log(listadeusuarios[i]);
+            $("."+listadeusuarios[i]+receptor).hide();
+        }
 
+        $("."+emisor+receptor).show();
+        alert("Nuevo mensaje de "+ emisor)
+    }
+    
     //Aqui registro el alias
   
     var contador=0;
@@ -83,6 +94,37 @@ $(document).ready(function() {
         alert(msg)
     });
     */
+    //si termina la conexion con la otra persona no pueden hablar, se borra el entorno creado para sus mensajes
+    $('#terminarconversacion').on('click', function() {
+        var alias = $('#terminarconversacion1').val();
+
+        alert("Se cerrara la conexion y el registro de datos  con el usuario!.");
+
+        $( 'div'+'.'+alias+yo ).append( '<div class="msg_bbl" style="margin-top: 15px; float: right; margin-left: 250px; ">'+ '<p class = "parrafodentro" style="color: red;" >Se ha terminado la conexion, no pueden seguir hablando!. </p>' + '</div>');
+        $('div.'+alias+yo).fadeOut(15000);
+        $('#'+alias+'b').remove();
+        //lo emite a flask para que la otra persona lo reciba en el private socket que esta justo aca abajo
+        private_socket.emit('borrarrastro',{'alias':alias,'yo':yo} );
+
+        //borrar todo lo que tenga que ver con ese alias y dsps con emit avisarle al otro para que no pueda mandar mas mensajes
+    });
+    //recibe el mensaje de que terminaron su conexion asi que se borra con .remove
+    private_socket.on('borrarrastro2', function(msg) {
+
+        //console.log('#'+msg.yo+'b' );
+       // 
+       alert("Se cerrara la conexion y el registro de datos  con el usuario!.");
+
+       $( 'div'+'.'+msg.yo+msg.alias ).append( '<div class="msg_bbl" style="margin-top: 15px; float: right; margin-left: 250px;">'+ '<p class = "parrafodentro" style="color: red;" >Se ha terminado la conexion, no pueden seguir hablando!. </p>'     + '</div>');
+           
+     
+       $('div.'+msg.yo+msg.alias).fadeOut(10000);
+        
+       $('#'+msg.yo+'b').remove();
+        //Tengo quqe agregar que terminaron su conexion y no puede enviarle mas mensajes
+
+        // 
+    });
 
     //Cuando un usuario envia el mensaje al otro
     $('#send_private_message').on('click', function() {
@@ -107,70 +149,61 @@ $(document).ready(function() {
       //  console.log(listadeusuarios);
         //comprobamos que no sea esta enviando mensajes a si mismo
         if(emisor !=recipient ){
+           
+            
+                //comprobamos que no se ha cerrado la sesion
+                if(listadeusuarios.indexOf(recipient)==-1){
+              
+                    //creo su propio holder con la clase del alias del emisor y a esa clase despues puedo hacerla desaparecer o aparecer
+                    $( 'div.message_holder' ).append( '<div class="'+recipient+emisor+ '"  style=""></div>');
+                    $( 'div'+'.'+recipient+emisor).append(' <div class="msg_bbl" style="margin-top: 15px; float: left; margin-right: 250px; ">'+'<p class = "parrafodentro" >'+message_to_send+'</p>'+ '<b style=" color: #000;font-size:10px; float: right;">   ' + hora+'</b></div>'+'<br>' );
+                    // $( 'div'+'.'+msg.emisor+' ).append( <div class="msg_bbl" style="margin-top: 15px; float: left; margin-right: 250px; "><p class = "parrafodentro" >'+message_to_send+'</p>'+ '<b style=" color: #000;font-size:10px; float: right;">   ' + hora+'</b></div>'+'<br>' );
+                    $( 'div.emisores' ).append( '<button class="btn btn-outline-success btn-block" id="'+recipient+'b" onclick=$("div.'+emisor+recipient+'").toggle();$("div.'+recipient+emisor+'").toggle();  >'+ "Alias :  "+ recipient +'</button>');
+                    listadeusuarios.push(recipient);
+                }
+                //Si ya hablaron antes solo agrega mensajes 
+                else{
+                    $( 'div'+'.'+recipient+emisor ).append(' <div class="msg_bbl"  style="margin-top: 15px; float: left; margin-right: 250px; ">'+'<p class = "parrafodentro" >'+message_to_send+'</p>'+ '<b style=" color: #000;font-size:10px; float: right;">   ' + hora+'</b></div>'+'<br>' );
+                
+                }
+    
+                //emite el mensaje privado a la otra persona para que se muestre en su pantalla, esto va a flask 
+                private_socket.emit('private_message', {'username' : recipient, 'message' : message_to_send,'emisor':emisor,'hora':hora});
+
+            }
+           
 
             //se comprueba si los usuarios hablan por primera vez
             // para crear una clase  y su" propio entorno de chat"
-            if(listadeusuarios.indexOf(recipient)==-1){
-              
-                //creo su propio holder con la clase del alias del emisor y a esa clase despues puedo hacerla desaparecer o aparecer
-                $( 'div.message_holder' ).append( '<div class="'+recipient+emisor+ '"  style=""></div>');
-                $( 'div'+'.'+recipient+emisor).append(' <div class="msg_bbl" style="margin-top: 15px; float: left; margin-right: 250px; ">'+'<p class = "parrafodentro" >'+message_to_send+'</p>'+ '<b style=" color: #000;font-size:10px; float: right;">   ' + hora+'</b></div>'+'<br>' );
-                // $( 'div'+'.'+msg.emisor+' ).append( <div class="msg_bbl" style="margin-top: 15px; float: left; margin-right: 250px; "><p class = "parrafodentro" >'+message_to_send+'</p>'+ '<b style=" color: #000;font-size:10px; float: right;">   ' + hora+'</b></div>'+'<br>' );
-                $( 'div.emisores' ).append( '<button class="btn btn-outline-success btn-block" id="'+recipient+'b" onclick=$("div.'+emisor+recipient+'").toggle();$("div.'+recipient+emisor+'").toggle();changeButtonColor("'+emisor+'b")   >'+ "Alias :  "+ recipient +'</button>');
-                listadeusuarios.push(recipient);
-            }
-            //Si ya hablaron antes solo agrega mensajes 
-            else{
-                $( 'div'+'.'+recipient+emisor ).append(' <div class="msg_bbl"  style="margin-top: 15px; float: left; margin-right: 250px; ">'+'<p class = "parrafodentro" >'+message_to_send+'</p>'+ '<b style=" color: #000;font-size:10px; float: right;">   ' + hora+'</b></div>'+'<br>' );
-            
-            }
-
-            //emite el mensaje privado a la otra persona para que se muestre en su pantalla, esto va a flask 
-            private_socket.emit('private_message', {'username' : recipient, 'message' : message_to_send,'emisor':emisor,'hora':hora});
-        }
-        else{
-            alert("No puedes hablar contigo mismo");
+           
         
-        }
+
       
 
        
     });
     
  
+    
 
-    //si termina la conexion con la otra persona no pueden hablar, se borra el entorno creado para sus mensajes
-    $('#terminarconversacion').on('click', function() {
-        var alias = $('#terminarconversacion1').val();
-        $('#'+alias+'b').remove();
-        $('div.'+alias+yo).remove();
-        //lo emite a flask para que la otra persona lo reciba en el private socket que esta justo aca abajo
-        private_socket.emit('borrarrastro',{'alias':alias,'yo':yo} );
-        //borrar todo lo que tenga que ver con ese alias y dsps con emit avisarle al otro para que no pueda mandar mas mensajes
-    });
-    //recibe el mensaje de que terminaron su conexion asi que se borra con .remove
-    private_socket.on('borrarrastro2', function(msg) {
-        console.log("AQUIIIIIII");
-        console.log('#'+msg.yo+'b' );
-        $('#'+msg.yo+'b').hide();
-        $('div.'+msg.yo+msg.alias).hide();
-        //Tengo quqe agregar que terminaron su conexion y no puede enviarle mas mensajes
-
-        //$( 'div'+'.'+msg.emisor+msg.receptor ).append( '<div class="msg_bbl" style="margin-top: 15px; float: right; margin-left: 250px;">'+ '<p class = "parrafodentro" >'+ msg.mensaje+ '</p>'+ '<b style="color: #000; font-size:10px; float: left;">'+ msg.hora+ '</b>'       + '</div>');
-            
-    });
 
 
     
     //Mensajes privados
 
-
+   
     
 //Cuando recibe el mensaje
     private_socket.on('new_private_message', function(msg) {
+
+        
    
         //Comprobamos si habiamos hablado antes para crear el entorno donde chatearan, esto en la pestaña del receptor
-        if(msg.hablando=="no"){       
+        if(msg.hablando=="no"){  
+                //hide todos los que tengan diferente clase a la que viene
+
+               
+
                 //como es primera vez que hablan se crea el entorno y sus botones
                 $( '#Mensajes' ).remove();
 
@@ -178,12 +211,13 @@ $(document).ready(function() {
                 $( 'div.message_holder' ).append( '<div class="'+msg.emisor+msg.receptor+ '"  style=""></div>');
                 $( 'div'+'.'+msg.emisor+msg.receptor ).append( '<div class="msg_bbl" style="margin-top: 15px; float: right; margin-left: 250px;">'+ '<p class = "parrafodentro" >'+ msg.mensaje+ '</p>'+ '<b style="color: #000; font-size:10px; float: left;">'+ msg.hora+ '</b>'   + '</div>');
                 a.push(msg.emisor);
-                $( 'div.emisores' ).append( '<button class="btn btn-outline-success btn-block" id="'+msg.emisor+'b" onclick=$("div.'+msg.receptor+msg.emisor+'").toggle();$("div.'+msg.emisor+msg.receptor+'").toggle(); changeButtonColor('+"+msg.emisor+'b"+') >'+ "Alias :  "+ msg.emisor +'</button>');
+                $( 'div.emisores' ).append( '<button class="btn btn-outline-success btn-block" id="'+msg.emisor+'b" onclick=$("div.'+msg.receptor+msg.emisor+'").toggle();$("div.'+msg.emisor+msg.receptor+'").toggle(); >'+ "Alias :  "+ msg.emisor +'</button>');
                
     
                 b = b+1; 
                 listadeusuarios.push(msg.emisor);
-            
+
+                ocultaratodos(msg.emisor,msg.receptor);
 
         }
         else{
